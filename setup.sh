2d8550d6a3e5529af4f8d2c4da4c61ce6ba42f2e
Kristoffer Flottorp <kristoffer.flottorp@nrk.no>
2019-04-03 15:03:32 +0200

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-echo -e "\n\033[1;31mInstalling python 2.7\033[0m"
-brew install python2
-
-echo -e "\n\033[1;31mInstalling python 3.x\033[0m"
+echo -e "\n\033[1;31mInstalling python 3\033[0m"
 brew install python3
 
 echo -e '\033[1;31mInstalling neovim\033[0m'
@@ -15,17 +12,11 @@ brew install the_silver_searcher
 echo -e "\n\033[1;31mInstalling thefuck\033[0m"
 brew install thefuck
 
-echo -e "\n\033[1;31mInstalling lolcat\033[0m"
-brew install lolcat
-
-echo -e "\n\033[1;31mInstalling fortune\033[0m"
-brew install fortune
-
-echo -e "\n\033[1;31mInstalling cowsay\033[0m"
-brew install cowsay
-
 echo -e "\n\033[1;31mInstalling swiftlint\033[0m"
 brew install swiftlint
+
+echo -e "\n\033[1;31mInstalling tree\033[0m"
+brew install tree
 
 echo -e "\n\033[1;31mTapping casks\033[0m"
 brew tap caskroom/cask
@@ -39,12 +30,30 @@ brew cask install karabiner-elements
 echo -e "\n\033[1;31mInstalling postman\033[0m"
 brew cask install postman
 
+echo -e "\n\033[1;31mInstalling brave\033[0m"
+brew cask install brave-browser
+
+echo -e "\n\033[1;31mInstalling visual studio code\033[0m"
+brew cask install visual-studio-code
+
+echo -e "\n\033[1;31mInstalling spotify\033[0m"
+brew cask install spotify
+
+echo -e "\n\033[1;31mInstalling slack\033[0m"
+brew cask install slack
+
+echo -e "\n\033[1;31mInstalling sublime-merge\033[0m"
+brew cask install sublime-merge
+
+echo -e "\n\033[1;31mInstalling 1password\033[0m"
+brew cask install 1password
+
+
 echo -e "\n\033[1;31mInstalling vim-plug\033[0m"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo -e "\n\033[1;31mEnabling python support for nvim\033[0m"
-pip2 install pynvim
 pip3 install pynvim
 
 echo -e "\n\033[1;31mTapping fonts\033[0m"
@@ -52,6 +61,9 @@ brew tap caskroom/fonts
 
 echo -e "\n\033[1;31mInstalling fira code\033[0m"
 brew cask install font-fira-code
+
+echo -e "\n\033[1;31mInstalling ia writer mono\033[0m"
+brew cask install font-ia-writer-mono
 
 echo -e "\n\033[1;31mCleaning up existing configurations\033[0m"
 rm -rf ~/.vim ~/.vimrc ~/.config/nvim ~/.bash_profile 2> /dev/null
