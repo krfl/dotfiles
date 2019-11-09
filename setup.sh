2575596b20a1b5a786a09c1ea3e516ed4232b1ee
krfl <kr.fl@outlook.com>
2019-11-10 00:38:42 +0100

    cleanup nvim stuff and move vim-plug installation to ~/.vim/

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-formulas=(python3 neovim the_silver_searcher thefuck tree mpv tldr tig)
+formulas=(python3 thefuck tree mpv tldr tig)
 casks=(iterm2 slack spotify drawio visual-studio-code)
 fonts=(font-fira-code)
 
@@ -26,12 +26,15 @@ do
     brew cask install ${var}
 done
 
-echo -e "\n\033[1:31mInstalling vim-plug\033[0m"
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo -e "\n\033[1:31mCreating directories\033[0m"
+-mkdir -p ~/.vim ~/.config
 
-echo -e "\n\033[1:31mEnabling python support for nvim\033[0m"
-pip3 install pynvim
+echo -e "\n\033[1:31mInstalling oh-my-zsh-\033[0m"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo -e "\n\033[1:31mInstalling vim-plug\033[0m"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo -e "\n\033[1:31mInstalling SF Mono\033[0m"
 cd /Applications/Utilities/Terminal.app/Contents/Resources/Fonts/
@@ -41,11 +44,8 @@ cd -
 echo -e "\n\033[1:31mCleaning up existing configurations\033[0m"
 rm -rf ~/.vim ~/.vimrc ~/.config/nvim ~/.bash_profile 2> /dev/null
 
-echo -e "\n\033[1:31mCreating directories\033[0m"
-mkdir -p ~/.config ~/.config/nvim
-
 echo -e "\n\033[1:31mSymlinking configurations\033[0m"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ln -s $DIR/vimrc ~/.config/nvim/init.vim
 ln -s $DIR/vimrc ~/.vimrc
 ln -s $DIR/bash_profile ~/.bash_profile
+ln -s $DIR/zshrc ~/.zshrc
