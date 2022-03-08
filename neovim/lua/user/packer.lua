07fb49914de39d3018c6fd4e277fb52ed1ac38a8
krfl <kr.fl@outlook.com>
2022-03-08 01:25:56 +0100

    Fix lowercase global

--- neovim/lua/user/packer.lua
+++ neovim/lua/user/packer.lua
@@ -2,7 +2,7 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
 -- Autocommand that reloads neovim whenever you save packer.nvim
@@ -87,7 +87,7 @@ return packer.startup(function(use)
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
-  if packer_bootstrap then
+  if PACKER_BOOTSTRAP then
     require('packer').sync()
   end
 end)
