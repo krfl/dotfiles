62a3ae520ecf050576a211b401d1814ada510a44
krfl <krfl@studio.lan>
2022-03-15 08:43:21 +0100

    fix merge conflict

--- neovim/lua/user/colorscheme.lua
+++ neovim/lua/user/colorscheme.lua
@@ -1,11 +1,7 @@
 vim.cmd [[
 try
-<<<<<<< Updated upstream
     set background=dark
     colorscheme fleetish
-=======
-  colorscheme deep-space
->>>>>>> Stashed changes
 catch /^Vim\%((\a\+)\)\=:E185/
     set background=dark
     colorsheme blue
--- neovim/lua/user/packer.lua
+++ neovim/lua/user/packer.lua
@@ -2,14 +2,10 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-<<<<<<< Updated upstream
   PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-=======
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 
 print "Installing packer close and reopen Neovim..."
 vim.cmd [[packadd packer.nvim]]
->>>>>>> Stashed changes
 end
 
 
