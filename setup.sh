3d723149e46e2b8c88262749c18ad4cbb62f533b
krfl <kr.fl@outlook.com>
2022-06-02 11:32:38 +0200

    update

--- setup.sh
+++ setup.sh
@@ -3,10 +3,14 @@
 echo -e "\n\033[1:31mInstalling Homebrew\033[0m"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo -e "\n\033[1:31mAdding Homebrew to default shellenv\033[0m"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 tools=(git fish starship neovim peco task ripgrep rust go node python3 docker)
 sugar=(exa bat tldr task tokei gitleaks apktool mas)
 casks=(kitty slack drawio rectangle)
-fonts=(font-cascadia-code)
+fonts=(font-caskaydia-cove-nerd-font font-agave-nerd-font font-jetbrains-mono-nerd-font)
 
 for var in "${tools[@]}"
 do
