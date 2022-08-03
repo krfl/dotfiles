15144c1314afedd4a9ddc84d996d05954119b57e
krfl <krfl@mobileoffice.lan>
2022-08-03 12:28:21 +0200

    temp. fix for new version of catppuccin (and comment)

--- neovim/lua/user/colorscheme.lua
+++ neovim/lua/user/colorscheme.lua
@@ -4,69 +4,69 @@ if not status_ok then
     return
 end
 
-catp.setup {
-    transparent_background = false,
-    term_colors = false,
-    styles = {
-        comments = "NONE",
-        functions = "NONE",
-        keywords = "NONE",
-        strings = "NONE",
-        variables = "NONE",
-    },
-    integrations = {
-    	treesitter = true,
-    	native_lsp = {
-    		enabled = true,
-    		virtual_text = {
-    			errors = "italic",
-    			hints = "italic",
-    			warnings = "italic",
-    			information = "italic",
-    		},
-    		underlines = {
-    			errors = "underline",
-    			hints = "underline",
-    			warnings = "underline",
-    			information = "underline",
-    		},
-    	},
-    	lsp_trouble = false,
-    	cmp = true,
-    	lsp_saga = false,
-    	gitgutter = false,
-    	gitsigns = true,
-    	telescope = true,
-    	nvimtree = {
-    		enabled = true,
-    		show_root = false,
-    		transparent_panel = false,
-    	},
-    	neotree = {
-    		enabled = false,
-    		show_root = false,
-    		transparent_panel = false,
-    	},
-        which_key = true,
-    	indent_blankline = {
-    		enabled = true,
-            colored_indent_levels = true,
-    	},
-    	dashboard = true,
-    	neogit = false,
-    	vim_sneak = false,
-    	fern = false,
-    	barbar = false,
-    	bufferline = true,
-    	markdown = true,
-    	lightspeed = false,
-    	ts_rainbow = false,
-    	hop = false,
-    	notify = true,
-    	telekasten = true,
-    	symbols_outline = true,
-    }
-}
+-- catp.setup {
+--     transparent_background = false,
+--     term_colors = false,
+--     styles = {
+--         comments = "NONE",
+--         functions = "NONE",
+--         keywords = "NONE",
+--         strings = "NONE",
+--         variables = "NONE",
+--     },
+--     integrations = {
+--     	treesitter = true,
+--     	native_lsp = {
+--     		enabled = true,
+--     		virtual_text = {
+--     			errors = "italic",
+--     			hints = "italic",
+--     			warnings = "italic",
+--     			information = "italic",
+--     		},
+--     		underlines = {
+--     			errors = "underline",
+--     			hints = "underline",
+--     			warnings = "underline",
+--     			information = "underline",
+--     		},
+--     	},
+--     	lsp_trouble = false,
+--     	cmp = true,
+--     	lsp_saga = false,
+--     	gitgutter = false,
+--     	gitsigns = true,
+--     	telescope = true,
+--     	nvimtree = {
+--     		enabled = true,
+--     		show_root = false,
+--     		transparent_panel = false,
+--     	},
+--     	neotree = {
+--     		enabled = false,
+--     		show_root = false,
+--     		transparent_panel = false,
+--     	},
+--         which_key = true,
+--     	indent_blankline = {
+--     		enabled = true,
+--             colored_indent_levels = true,
+--     	},
+--     	dashboard = true,
+--     	neogit = false,
+--     	vim_sneak = false,
+--     	fern = false,
+--     	barbar = false,
+--     	bufferline = true,
+--     	markdown = true,
+--     	lightspeed = false,
+--     	ts_rainbow = false,
+--     	hop = false,
+--     	notify = true,
+--     	telekasten = true,
+--     	symbols_outline = true,
+--     }
+-- }
 
 vim.cmd [[
 try
