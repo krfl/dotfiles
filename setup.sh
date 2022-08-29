b14c00fdca5f5b763f266e0c6c207e988c78cdf1
krfl <kr.fl@outlook.com>
2022-08-29 09:47:59 +0200

    update

--- setup.sh
+++ setup.sh
@@ -7,12 +7,11 @@ echo -e "\n\033[1:31mAdding Homebrew to default shellenv\033[0m"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-tools=(git starship elvish helix peco task ripgrep rustup go node python3 docker)
+tools=(git starship elvish peco task ripgrep rustup go node python3 docker carapace)
 sugar=(exa bat tldr task tokei gitleaks apktool mas imagemagick lazygit)
 casks=(kitty slack drawio rectangle)
 fonts=(font-caskaydia-cove-nerd-font font-jetbrains-mono-nerd-font)
 
-brew tap helix-editor/helix
 brew tap homebrew/cask-fonts
 
 for var in "${tools[@]}"
