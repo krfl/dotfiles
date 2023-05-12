4e262dac26910bb0d64404ca5417ffed9de8cedc
krfl <kr.fl@outlook.com>
2023-05-12 02:26:31 +0200

    chore: remove cleanup script

--- cleanup.sh
+++
@@ -1,8 +0,0 @@
-
-rm -rf ~/.config/kitty/ 2> /dev/null
-rm -rf ~/.config/bat/ 2> /dev/null
-rm -rf ~/.config/helix/ 2> /dev/null
-rm -rf ~/.config/peco/ 2> /dev/null
-rm -f ~/.config/starship.toml 2> /dev/null
-rm -rf ~/.config/fish/ 2> /dev/null
-rm -f ~/.zshrc 2> /dev/null
