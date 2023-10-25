0d27d8201e081834f15a118435decda19177381f
krfl <kr.fl@outlook.com>
2023-10-25 11:07:27 +0200

    chore: stop using deprecated eza options

--- fish/.config/fish/functions/ll.fish
+++ fish/.config/fish/functions/ll.fish
@@ -1,5 +1,5 @@
 # ll alias using exa
 # 'short form' ls -l sorted by type
 function ll
-    command eza --sort=type --long --git --no-icons --no-permissions --no-user --no-time $argv
+    command eza --sort=type --long --git --no-permissions --no-user --no-time $argv
 end
