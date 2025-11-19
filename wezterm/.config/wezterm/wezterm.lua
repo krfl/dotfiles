eef2466acbe5bfa1cf94834b563e95c8487f141e
krfl <kr.fl@outlook.com>
2025-11-19 07:55:22 +0100

    correct light and dark themes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,10 +8,9 @@ local fsize = 15
 -- adapt to os theme
 config.color_scheme_dirs = { '~/.config/wezterm/colors' }
 if appearance.is_dark() then
-  -- config.color_scheme = 'vesper2'
   config.color_scheme = 'serene-night'
 else
-  config.color_scheme = 'flatwhite'
+  config.color_scheme = 'serene-day'
 end
 
 -- window
